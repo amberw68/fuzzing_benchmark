b0323dfda7b5352f82c08d5a210f09f53e701161
Yifan <yw03021103@gmail.com>
2022-11-17 00:16:01 -0500

    update bug in pdf-object.c

--- possible_progs/final_version/mupdf-1.20.3-source/source/pdf/pdf-object.c
+++ possible_progs/final_version/mupdf-1.20.3-source/source/pdf/pdf-object.c
@@ -751,10 +751,10 @@ discard_journal_entries(fz_context *ctx, pdf_journal_entry **journal_entry)
 	{
 		pdf_journal_entry *next = entry->next;
 
+		//YIFAN : added line 755/756
+		int length = strlen(entry->head);
+		*(entry->head + length) = 'a';
 		discard_fragments(ctx, entry->head);
-		//YIFAN : added line 756/757
-		int length = strlen(entry->title);
-		*(entry->title + length) = 'a';
 		fz_free(ctx, entry->title);
 		fz_free(ctx, entry);
 		entry = next;
